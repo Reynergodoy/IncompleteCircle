a29d76a6b77df720ff6b88c9e92163e2694cbd2c
Rey <reyneraccel@gmail.com>
2018-10-02 17:53:25 -0300

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,6 @@ let circle = new IncompleteCircle(100, 140, 30);
 
 console.log(circle.getDimensions());
 // (2) [22.981333293569335, 29.544232590366242]
+
+console.log(circle.allQuadrants());
+// [2]
