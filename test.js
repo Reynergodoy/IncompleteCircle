5814b8c15f9e2bff63a95b98c1ea72cc4da24936
Rey <reyneraccel@gmail.com>
2018-10-02 17:44:37 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+import { IncompleteCircle } from 'incomplete.js';
+
+let circle = new IncompleteCircle(100, 140, 30);
+
+console.log(circle.getDimensions());
+// (2) [22.981333293569335, 29.544232590366242]
