9185091a98d621b66a4e929d389b9686ceabe71d
Rey <reyneraccel@gmail.com>
2019-01-22 20:13:57 -0200

    Update incomplete.js

--- src/incomplete.js
+++ src/incomplete.js
@@ -1,7 +1,7 @@
 import { Circle } from 'circle.js';
 
 export class IncompleteCircle {
-    constructor(start, end, radius){
+    constructor (start, end, radius) {
         this.start = start;
         this.end = end;
         this.radius = radius;
@@ -15,11 +15,11 @@ export class IncompleteCircle {
     allQuadrants () {
         const startQuad = Circle.getQuadrant(this.start);
         const endQuad = Circle.getQuadrant(this.end);
-        let quadrants = [startQuad];
-        if(( Circle.getNatural(this.start) <= Circle.getNatural(this.end) ) && ( startQuad === endQuad )) return quadrants;
-        let i = ( startQuad + 1 );
-        while((i % 5) !== endQuad){
-            if(i % 5 !== 0) quadrants.push(i % 5);
+        const quadrants = [startQuad];
+        if (( Circle.getNatural(this.start) <= Circle.getNatural(this.end) ) && ( startQuad === endQuad )) return quadrants;
+        let i = startQuad + 1;
+        while ((i % 5) !== endQuad) {
+            if (i % 5 !== 0) quadrants.push(i % 5);
             i++;
         }
         quadrants.push(endQuad);
@@ -27,58 +27,58 @@ export class IncompleteCircle {
     }
     
     getDimensions () {
-        if(this.start === this.end) return null;
+        if (this.start === this.end) return null;
         const quadrants = this.allQuadrants();
         const quantity = quadrants.length;
         const start = quadrants[0];
         const radius = this.radius;
         const firstAngle = Circle.getRadians(this.start);
         const lastAngle = Circle.getRadians(this.end);
-        if(quantity === 1){
-            if(start === 1 || start === 3){
+        if (quantity === 1) {
+            if (start === 1 || start === 3) {
                 const xWing = radius * Math.abs(Math.cos(firstAngle));
                 const yWing = radius * Math.abs(Math.sin(lastAngle));
                 return [xWing, yWing];
             }
-            if(start === 2 || start === 4){
+            if (start === 2 || start === 4) {
                 const xWing = radius * Math.abs(Math.cos(lastAngle));
                 const yWing = radius * Math.abs(Math.sin(firstAngle));
                 return [xWing, yWing];
             }
         }
-        if(quantity === 2){
-            if(start === 1 || start === 3){
+        if (quantity === 2) {
+            if (start === 1 || start === 3) {
                 const wing = Math.abs(Math.cos(firstAngle)) + Math.abs(Math.cos(lastAngle));
                 return [wing * radius, radius];
             }
-            if(start === 2 || start === 4){
+            if (start === 2 || start === 4) {
                 const wing = Math.abs(Math.sin(firstAngle)) + Math.abs(Math.sin(lastAngle));
                 return [radius, wing * radius];
             }
         }
-        if(quantity === 3){
-            if(start === 1 || start === 3){
+        if (quantity === 3) {
+            if (start === 1 || start === 3) {
                 const xWing = radius * ( 1 + Math.abs(Math.cos(firstAngle)) );
                 const yWing = radius * ( 1 + Math.abs(Math.sin(lastAngle)) );
                 return [xWing, yWing];
             }
-            if(start === 2 || start === 4){
+            if (start === 2 || start === 4) {
                 const xWing = radius * ( 1 + Math.abs(Math.cos(lastAngle)) );
                 const yWing = radius * ( 1 + Math.abs(Math.sin(firstAngle)) );
                 return [xWing, yWing];
             }
         }
-        if(quantity === 4){
-            if(start === 1 || start === 3){
+        if (quantity === 4) {
+            if (start === 1 || start === 3) {
                 const wing = Math.max(Math.abs(Math.cos(firstAngle)), Math.abs(Math.cos(lastAngle)));
                 return [radius * ( 1 + wing ), 2 * radius];
             }
-            if(start === 2 || start === 4){
+            if (start === 2 || start === 4) {
                 const wing = Math.max(Math.abs(Math.sin(firstAngle)), Math.abs(Math.sin(lastAngle)));
                 return [2 * radius, radius * ( 1 + wing )];
             }
         }
-        if(quantity === 5){
+        if (quantity === 5) {
             return [2 * radius, 2 * radius];
         }
     }
